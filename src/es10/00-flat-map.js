07a5192670af0f488a65ac5203ddee52dc71c762
Ramiro Uffelmann <ramirouf@gmail.com>
2023-04-07 19:36:38 -0300

    Finished 24th class (flatmap and trim)

---
+++ src/es10/00-flat-map.js
@@ -0,0 +1,7 @@
+//flat
+const array = [1, 2, 3, 4, 5, [6, 7, 8, [9, 10, 11]]];
+console.log(array.flat(2));
+
+//flatmap
+const array2 = [1, 2, 3, 4];
+console.log(array2.flatMap((v) => [v, v * 2]));
