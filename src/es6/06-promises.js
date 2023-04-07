044e14a710bc70f53a16e57370952db9129a5241
Ramiro Uffelmann <ramirouf@gmail.com>
2023-04-07 12:39:46 -0300

    Finished 11th class (Promises)

---
+++ src/es6/06-promises.js
@@ -0,0 +1,13 @@
+const func = () => {
+    return new Promise((resolve, reject) => {
+        if (false) {
+            resolve("Solved");
+        } else {
+            reject("Failed");
+        }
+    })
+}
+
+func()
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
