a9e3b681f8486ea20c73976eb90b6f97551412d9
Ramiro Uffelmann <ramirouf@gmail.com>
2023-04-07 12:22:39 -0300

    Finished 8th class (Rest params)

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -9,3 +9,17 @@ console.log(a, fruits[1]);
 let user = { username: "Ramiro", age: 21 };
 let { username, age } = user;
 console.log(username, user.age);
+
+//Spread operator
+let person = { name: "Ramiro", age: 21 };
+let country = "ARG";
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// Rest params (usefull to receive many params)
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+sum(1, 1, 2, 3);
