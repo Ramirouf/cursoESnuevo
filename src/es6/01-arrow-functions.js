cd5799546c821dec1dc5135be112eb495e389d4c
Ramiro Uffelmann <ramirouf@gmail.com>
2023-04-07 12:00:27 -0300

    Finished 4th class (let, const, and arrow functions)

---
+++ src/es6/01-arrow-functions.js
@@ -0,0 +1,12 @@
+//Normal way of creating functions
+function square(num) {
+    return num * num;
+}
+
+//Normal arrow function
+const square = (num) => {
+    return num * num;
+}
+
+//Arrow function with one parameter and one line of code
+const square = num => num * num;
